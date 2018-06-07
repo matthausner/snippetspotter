fd11d74698be0e388d5cb0c6674d590c34cdaff9
Matthias Hausner <m.hausner@protonmail.com>
2018-06-07 23:30:28 +0200

    replaced composer with Core.php

---
+++ Core.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'jwilsson/spotify-web-api-php/src/Request.php';
+    require_once 'jwilsson/spotify-web-api-php/src/SpotifyWebAPI.php';
+    require_once 'jwilsson/spotify-web-api-php/src/SpotifyWebAPIException.php';
+    require_once 'jwilsson/spotify-web-api-php/src/Session.php';
+?>
--- SpotifyAccessTokenFactory.php
+++ SpotifyAccessTokenFactory.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require 'Core.php';
 
 class SpotifyAccessTokenFactory {
 
