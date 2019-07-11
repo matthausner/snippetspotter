ca0e7bf8bebd19090ab118f51455a5b819acac3b
Matthias Hausner <m.hausner@protonmail.com>
2019-07-12 01:41:48 +0200

    removed reference to Core.php, added requirement

--- Core.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/Request.php';
-    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/SpotifyWebAPI.php';
-    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/SpotifyWebAPIException.php';
-    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/Session.php';
-?>
