9643606333966288102f521e88e6761ba84fa66b
Matthias Hausner <m.hausner@protonmail.com>
2019-08-04 09:55:29 +0200

    Revert "removed reference to Core.php, added requirement"
    This reverts commit 5c69a2595accfc77cff053d9c0263c0449d38c76.

---
+++ Core.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/Request.php';
+    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/SpotifyWebAPI.php';
+    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/SpotifyWebAPIException.php';
+    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/Session.php';
+?>
