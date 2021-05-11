da6d3c87f8141ec1d910e379ea266b5d723f6b66
Felix Bartling <reditec@outlook.com>
2021-05-11 10:32:27 +0200

    dependencies suck!

--- Core.php
+++ Core.php
@@ -1,6 +1,3 @@
 <?php
-    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/Request.php';
-    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/SpotifyWebAPI.php';
-    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/SpotifyWebAPIException.php';
-    require_once 'imports/extensions/jwilsson/spotify-web-api-php/src/Session.php';
+    /* Dependencies can be imported via composer :-) */
 ?>
